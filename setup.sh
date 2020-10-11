f0ff545274c2cb26296fb0b24e61143d1deb686c
Sachin Saini <sachin.saini@ap2v.com>
2020-10-11 13:29:32 +0530

    add docker-compose file and modify setup file;

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# map all hosts in /etc/hosts
+
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+docker-compose up -d
